a3f5e8742fcdca0807944d7eb62bdd6735158fad
Maria Camila Pulido Bedoya <mpulidobe@unal.edu.co>
2026-07-11 22:28:54 -0500

    parametros del modelo continuo 20L

--- simulaciones/Continuo20L.py
+++ simulaciones/Continuo20L.py
@@ -1,1 +1,30 @@
+'''Cultivo continuo con recirculacion de biomasa biorreactor 20L'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+#Parametros
+Kd = 0.0001 #coeficiente de muerte celular [h^-1]
+miu_max = 1.09 #tasa de crecimiento especifica maxima [h^-1]
+qs_max = 4.16 #tasa de utilizacion de sustrato especifica maxima [g/g*h]
+qp_max = 1.863 #tasa de produccion de lactato especifica maxima [g/g*h]
+alpha = 0.017 #constante asociada al crecimiento en Luedeking-Piret [g/g]
+
+#Constantes de limitacion de sustrato
+Ksx = 4.229 #Limitacion de sustrato para el crecimiento de la biomasa [g/L]
+Kss = 0.15 #Limitacion de sustrato para el consumo de sustrato [g/L]
+Ksp = 0.065 #Limitacion de sustrato para la produccion de lactato [g/L]
+
+#Constantes de inhibicion por sustrato
+Kix = 394.20 #Inhibicion del sustrato para el crecimiento de la biomasa [g/L]
+Kis = 143.391 #Inhibicion del sustrato para el consumo de sustrato [g/L]
+Kip = 373.89 #Inhibicion del sustrato para la produccion de lactato [g/L]
+
+#Constantes de inhibicion por producto
+Kpx = 5.001 #Inhibicion del producto para el crecimiento de la biomasa [g/L]                 
+Kps = 20.07 #Inhibicion del producto para el consumo de sustrato [g/L]
+Kpp = 42.83 #Inhibicion del producto para la produccion de lactato [g/L]
+
+#Rendimientos (Ypx, Yps, Yxs)
 
